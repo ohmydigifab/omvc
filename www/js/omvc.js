a5fc3525fbb51758974d9384c5b91ab65faef205
takumadx <hirayama.takuma@ohmydigifab.com>
2015-12-16 22:38:43 +0900

    drive mode

--- www/js/omvc.js
+++ www/js/omvc.js
@@ -19,11 +19,18 @@ function OMVC() {
 		Yaw : 0
 	};
 
+	var ViewModeEnum = {
+		Dive : 0,
+		Drive : 1
+	};
+
 	var overlayElement = document.getElementById("overlay");
 	overlayElement.hidden = true;
-	
+
 	var throttleNode = document.createTextNode("");
 	var debugMsgNode = document.createTextNode("");
+
+	var viewMode = ViewModeEnum.Dive;
 
 	var myself = {
 		set_infobox_enabled : function(value) {
@@ -37,7 +44,6 @@ function OMVC() {
 			} else {
 				myAttitude.Yaw -= myAttitude_init.Yaw;
 			}
-			myself.omvr.set_myAttitude(myAttitude);
 		},
 		set_vehicleAttitude : function(value) {
 			vehicleAttitude = value;
@@ -46,7 +52,6 @@ function OMVC() {
 			} else {
 				vehicleAttitude.Yaw -= vehicleAttitude_init.Yaw;
 			}
-			myself.omvr.set_vehicleAttitude(vehicleAttitude);
 		},
 		omvr : new OMVR(),
 		init : function() {
@@ -57,19 +62,23 @@ function OMVC() {
 			// Add those text nodes where they need to go
 			throttleElement.appendChild(throttleNode);
 			debugMsgElement.appendChild(debugMsgNode);
-			
+
 			jQuery.getScript("http://192.168.42.1:9001/socket.io/socket.io.js", function() {
 				socket = io.connect('http://192.168.42.1:9001');
 				// サーバから受け取るイベント
 				socket.on('connect', function() {
 					setInterval(function() {
 						var _starttime = new Date();
-						// console.log('ping!!');
+						console.log('ping!!');
 						socket.emit('ping', _starttime);
 					}, 500);
 				});
-				socket.on('pong', function() {
-					// console.log('pong!!');
+				socket.on('pong', function(obj) {
+					console.log('pong!!');
+					console.log(obj);
+					if (obj.FlightStatus.Armed) {
+						console.log("Armed");
+					}
 				});
 				socket.on('msg', function(msg) {
 					console.log('msg:' + msg);
@@ -87,7 +96,7 @@ function OMVC() {
 				}
 				socket.emit('getAttitude', function(obj) {
 					// console.log(obj);
-					set_vehicleAttitude(obj);
+					myself.set_vehicleAttitude(obj);
 					debug_msg = myAttitude.Roll.toFixed(0) + "," + myAttitude.Pitch.toFixed(0) + "," + myAttitude.Yaw.toFixed(0);
 					debug_msg += "\n" + vehicleAttitude.Roll.toFixed(0) + "," + vehicleAttitude.Pitch.toFixed(0) + "," + vehicleAttitude.Yaw.toFixed(0);
 				});
@@ -107,7 +116,17 @@ function OMVC() {
 		},
 
 		animate : function() {
-			myself.omvr.set_myAttitude(myAttitude);
+			if (viewMode == ViewModeEnum.Dive) {
+				myself.omvr.set_myAttitude(myAttitude);
+				myself.omvr.set_vehicleAttitude(vehicleAttitude);
+			} else {
+				myself.omvr.set_myAttitude(myAttitude);
+				myself.omvr.set_vehicleAttitude({
+					Roll : 90,
+					Pitch : 0,
+					Yaw : 0
+				});
+			}
 			myself.omvr.animate();
 			{// status
 				throttleNode.nodeValue = (throttle * 100).toFixed(0) + "%";
@@ -117,19 +136,29 @@ function OMVC() {
 			requestAnimationFrame(myself.animate);
 		},
 
-		arm : function(value) {
+		setArm : function(value) {
 			if (socket == null) {
 				return;
 			}
-			if (value) {
-				socket.emit('arm', function() {
-					throttle = 0;
+			socket.emit('setThrottle', 1000, function(res) {
+				socket.emit('setRoll', 1500, function(res) {
+					socket.emit('setPitch', 1500, function(res) {
+						socket.emit('setYaw', 1500, function(res) {
+							socket.emit('setArm', value, function(res) {
+								throttle = 0;
+							});
+						});
+					});
 				});
-			} else {
-				socket.emit('disarm', function() {
-					throttle = 0;
-				});
-			}
+			});
+		},
+
+		setDiveMode : function() {
+			viewMode = ViewModeEnum.Dive;
+		},
+
+		setDriveMode : function() {
+			viewMode = ViewModeEnum.Drive;
 		}
 	};
 	return myself;
